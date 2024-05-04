0605e948c14d08e2d57420c7b1691ca4f5aa040b
CODER-AMLAN <145832307+CODER-AMLAN@users.noreply.github.com>
2024-05-04 22:43:40 +0530

    Update P2.java

--- DSA ASSIGNMENT 3/DSA CLASS ASSIGNMENT 3/P2.java
+++ DSA ASSIGNMENT 3/DSA CLASS ASSIGNMENT 3/P2.java
@@ -1,10 +1,24 @@
+/*Question 2. 
+Assign your favorite colors in an array. Identify 2 exceptions that may be generated & write
+exceptional handler in Java.*/
+
 import java.util.Scanner;
 
 public class P2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[]color = new String[5];
-        System.out.println("Enter your favourite colours");
-        sc.close();
-    }    
+        try {
+            String[] color = { "Violet", "Black", "Green", "Blue", "Yellow" };
+            System.out.println("Enter index: ");
+            int idx = sc.nextInt();
+            if (idx < 0)
+                throw new NumberFormatException("Index cannot be negative");
+            System.out.print("Colour at index " + idx + " is: " + color[idx]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+        }
+        sc.close();//OPTIONAL
+    }
 }
