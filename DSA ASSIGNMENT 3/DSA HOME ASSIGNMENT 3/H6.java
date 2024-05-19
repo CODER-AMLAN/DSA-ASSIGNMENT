2ddc437ac938e50c64ae255b10ece5beeee524cf
CODER-AMLAN <145832307+CODER-AMLAN@users.noreply.github.com>
2024-05-19 10:23:53 +0530

    Update H6.java

--- DSA ASSIGNMENT 3/DSA HOME ASSIGNMENT 3/H6.java
+++ DSA ASSIGNMENT 3/DSA HOME ASSIGNMENT 3/H6.java
@@ -3,9 +3,24 @@
 equal to its reverse. Examples of palindromes include 'racecar' and
 'gohangasalamiimalasagnahog'.
  */
+import java.util.Scanner;
 public class H6 {
-    public static void main(String[] args) {
-        
-    }
+	public static boolean checkPalindrome(String s,int i,int f){
+		  if(i==f||(i+f)!=s.length())
+		       return s.charAt(i)==s.charAt(f);
+		    if(s.charAt(i)!=s.charAt(f))
+		       return false;
+		   else
+		        return  checkPalindrome(s,i+1,f-1);
+		}
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in) ;
+	    System.out.print("Enter the String: ");
+	    String s = sc.nextLine();
+	      if(checkPalindrome(s,0,s.length()-1))
+	          System.out.println("Palindrome.");
+	          else
+	             System.out.println("Not a Palindrome.");
+	}
 }
 //WILL BE UPLOADED SOON... 
